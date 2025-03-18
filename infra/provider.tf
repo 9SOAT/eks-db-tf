1a4da319ab6a69cdbb23e7379c184d2f5b45f0d6
francesquini88 <68014943+francesquini88@users.noreply.github.com>
2025-03-18 00:19:25 -0300

    Update provider.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
   region     = us-east-1
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}  # Use variáveis ou um gerenciador de segredos
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}  # Use variáveis ou um gerenciador de segredos
 }
