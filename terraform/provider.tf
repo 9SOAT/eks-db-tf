5242e637fd598415280b0d2e8471155c06ff8847
= <=>
2025-03-18 09:22:24 -0300

    update file terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key  # Use variáveis ou um gerenciador de segredos
-  secret_key = var.aws_secret_key  # Use variáveis ou um gerenciador de segredos
+  region     = "us-east-1"
 }
