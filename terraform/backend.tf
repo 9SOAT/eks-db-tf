644ca9bb92c5e523d44a781a4b6dfb72976ca107
= <=>
2025-03-20 14:48:03 -0300

    adicionando regiao s3 terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "terraform-state-matheus-fiap"
     key            = "tfstate/terraform.tfstate"
-    region         = "us-east-2"
+    region         = var.awsRegion
     encrypt        = true
   }
 }
