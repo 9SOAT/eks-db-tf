7c0c761bb7ebf40f6bfa284ad08923fa86120f88
Victor Ambiel <113382997+ambielvictor@users.noreply.github.com>
2025-03-20 23:47:10 -0300

    Remove atributo consumer_id

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,11 +48,6 @@ resource "aws_dynamodb_table" "users" {
     type = "S"
   }
 
-  attribute {
-    name = "consumer_id"
-    type = "N"
-  }
-
   tags = {
   Name        = "DinamoDB"
   Environment = "Dev"
